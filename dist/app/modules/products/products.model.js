15e322ed971f88f582c053649ce5aad7c2a804e4
AbuBokorprog <abubokor1066@gmail.com>
2024-07-09 12:29:43 +0600

    Products interface & model created

---
+++ dist/app/modules/products/products.model.js
@@ -0,0 +1,69 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importStar(require("mongoose"));
+const ProductsSchema = new mongoose_1.default.Schema({
+    product_name: {
+        types: String,
+        required: true,
+        unique: true,
+    },
+    image: {
+        types: String,
+        required: true,
+    },
+    category: {
+        types: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        types: String,
+        required: true,
+    },
+    brand: {
+        types: String,
+        required: true,
+        unique: true,
+    },
+    price: {
+        types: Number,
+        required: true,
+    },
+    product_description: {
+        types: String,
+        required: true,
+    },
+    rating: {
+        types: Number,
+        required: true,
+    },
+    stock_quantity: {
+        types: Number,
+        required: true,
+    },
+});
+const Products = (0, mongoose_1.model)('product', ProductsSchema);
+exports.default = Products;
---
+++ src/app/modules/products/products.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { model } from 'mongoose'
+import TProducts from './products.services'
+
+const ProductsSchema = new mongoose.Schema<TProducts>({
+  product_name: {
+    types: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    types: String,
+    required: true,
+  },
+  category: {
+    types: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    types: String,
+    required: true,
+  },
+  brand: {
+    types: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    types: Number,
+    required: true,
+  },
+  product_description: {
+    types: String,
+    required: true,
+  },
+  rating: {
+    types: Number,
+    required: true,
+  },
+  stock_quantity: {
+    types: Number,
+    required: true,
+  },
+})
+
+const Products = model<TProducts>('product', ProductsSchema)
+
+export default Products
