ad67555f2f41bff61a799aa8a734f838a13c845d
AbuBokorprog <abubokor1066@gmail.com>
2024-07-09 12:50:03 +0600

    product validation schema created

--- dist/app/modules/products/products.validation.js
+++ dist/app/modules/products/products.validation.js
@@ -1,1 +1,28 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.updateProductValidationSchema = exports.createdProductValidationSchema = void 0;
+const zod_1 = require("zod");
+const createdProductValidationSchema = zod_1.z.object({
+    product_name: zod_1.z.string(),
+    image: zod_1.z.string(),
+    category: zod_1.z.string(),
+    description: zod_1.z.string(),
+    brand: zod_1.z.string(),
+    price: zod_1.z.number().positive(),
+    product_description: zod_1.z.string(),
+    rating: zod_1.z.number().min(0).max(5),
+    stock_quantity: zod_1.z.number().int().nonnegative(),
+});
+exports.createdProductValidationSchema = createdProductValidationSchema;
+const updateProductValidationSchema = zod_1.z.object({
+    product_name: zod_1.z.string().optional(),
+    image: zod_1.z.string().optional(),
+    category: zod_1.z.string().optional(),
+    description: zod_1.z.string().optional(),
+    brand: zod_1.z.string().optional(),
+    price: zod_1.z.number().positive().optional(),
+    product_description: zod_1.z.string().optional(),
+    rating: zod_1.z.number().min(0).max(5).optional(),
+    stock_quantity: zod_1.z.number().int().nonnegative().optional(),
+});
+exports.updateProductValidationSchema = updateProductValidationSchema;
---
+++ src/app/modules/products/products.validation.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+
+const createdProductValidationSchema = z.object({
+  product_name: z.string(),
+  image: z.string(),
+  category: z.string(),
+  description: z.string(),
+  brand: z.string(),
+  price: z.number().positive(),
+  product_description: z.string(),
+  rating: z.number().min(0).max(5),
+  stock_quantity: z.number().int().nonnegative(),
+})
+
+const updateProductValidationSchema = z.object({
+  product_name: z.string().optional(),
+  image: z.string().optional(),
+  category: z.string().optional(),
+  description: z.string().optional(),
+  brand: z.string().optional(),
+  price: z.number().positive().optional(),
+  product_description: z.string().optional(),
+  rating: z.number().min(0).max(5).optional(),
+  stock_quantity: z.number().int().nonnegative().optional(),
+})
+
+export { createdProductValidationSchema, updateProductValidationSchema }
