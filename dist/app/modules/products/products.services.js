59fb212ba2d2bc6dfb6aef84d02e34555ee48da2
AbuBokorprog <abubokor1066@gmail.com>
2024-07-10 14:41:53 +0600

    productServices error handle completed

--- dist/app/modules/products/products.services.js
+++ dist/app/modules/products/products.services.js
@@ -4,17 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.productsServices = void 0;
+const http_status_1 = __importDefault(require("http-status"));
+const AppError_1 = require("../../error/AppError");
 const products_model_1 = __importDefault(require("./products.model"));
 const createProductIntoDB = async (payload) => {
     const result = await products_model_1.default.create(payload);
+    if (!result) {
+        throw new AppError_1.AppError(http_status_1.default.BAD_REQUEST, 'Created product failed!');
+    }
     return result;
 };
 const retrieveAllProducts = async () => {
     const result = await products_model_1.default.find();
+    if (!result || result?.length <= 0) {
+        throw new AppError_1.AppError(http_status_1.default.BAD_REQUEST, 'Product not found!');
+    }
     return result;
 };
 const retrieveSingleProduct = async (id) => {
     const result = await products_model_1.default.findById(id);
+    if (!result) {
+        throw new AppError_1.AppError(http_status_1.default.BAD_REQUEST, 'Product not found!');
+    }
     return result;
 };
 const updateSingleProduct = async (id, payload) => {
@@ -22,10 +33,16 @@ const updateSingleProduct = async (id, payload) => {
         new: true,
         runValidators: true,
     });
+    if (!result) {
+        throw new AppError_1.AppError(http_status_1.default.BAD_REQUEST, 'Update product failed!');
+    }
     return result;
 };
 const deleteSingleProduct = async (id) => {
     const result = await products_model_1.default.findByIdAndDelete(id);
+    if (!result) {
+        throw new AppError_1.AppError(http_status_1.default.BAD_REQUEST, 'Delete product failed!');
+    }
     return result;
 };
 exports.productsServices = {
--- src/app/modules/products/products.services.ts
+++ src/app/modules/products/products.services.ts
@@ -1,18 +1,32 @@
+import httpStatus from 'http-status'
+import { AppError } from '../../error/AppError'
 import TProducts from './products.interface'
 import Products from './products.model'
 
 const createProductIntoDB = async (payload: TProducts) => {
   const result = await Products.create(payload)
 
+  if (!result) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Created product failed!')
+  }
+
   return result
 }
 const retrieveAllProducts = async () => {
   const result = await Products.find()
 
+  if (!result || result?.length <= 0) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Product not found!')
+  }
+
   return result
 }
 const retrieveSingleProduct = async (id: string) => {
   const result = await Products.findById(id)
+
+  if (!result) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Product not found!')
+  }
 
   return result
 }
@@ -22,10 +36,18 @@ const updateSingleProduct = async (id: string, payload: Partial<TProducts>) => {
     runValidators: true,
   })
 
+  if (!result) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Update product failed!')
+  }
+
   return result
 }
 const deleteSingleProduct = async (id: string) => {
   const result = await Products.findByIdAndDelete(id)
+
+  if (!result) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Delete product failed!')
+  }
 
   return result
 }
